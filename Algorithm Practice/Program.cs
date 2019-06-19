2431cf5a4b30502f80f912c6c35ac133185e6e21
donjuanwu <36903241+donjuanwu@users.noreply.github.com>
2019-06-19 15:24:49 -0700

    Started to practice for interview. Reviewed : Bitwise left/rigth shift, Paritycheck, Boxing/Unboxing

--- Algorithm Practice/Program.cs
+++ Algorithm Practice/Program.cs
@@ -8,11 +8,25 @@ namespace Algorithm_Practice
         static void Main(string[] args)
         {
             Console.WriteLine(ASTERISK + "Interview Preparation" + ASTERISK);
-            PracticeAlgorithm();
+           // PracticeAlgorithm();
+          //  TestBoxing();
+          //  TestUnBoxing();
+          //  TestBitWiseShift();
+            TestParity();
 
            
 
             Console.ReadKey();
+        }
+
+        public static void Intro()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(ASTERISK + "Begin Display Topics To Be Covered" + ASTERISK);
+
+
+            Console.WriteLine("");
+            Console.WriteLine(ASTERISK + "End Display Topics To Be Covered" + ASTERISK);
         }
 
 
@@ -84,5 +98,105 @@ namespace Algorithm_Practice
                 }
             }
         }
+
+        //The process of Converting a Value Type (char, int, etc.) to a Reference Type (object) is called Boxing
+        //Boxing is implicit conversion process in which object type (super type) is used
+        //The Value type is always stored in Stack. 
+        //The Referenced Type is stored in Heap
+        public static void TestBoxing()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(ASTERISK + "Begin display Boxing" + ASTERISK);
+
+            int num = 23; //declare on the stack
+            Object obj = num; //Boxing: assigned a value type (int) to a reference type (object)
+
+          //  num = 50; //assign 50 to num
+            
+            Console.WriteLine("Value type - value {0}", num);
+            Console.WriteLine("Reference type - value {0}", obj);
+
+            Console.WriteLine("");
+            Console.WriteLine(ASTERISK + "End display Boxing" + ASTERISK);
+
+        }
+
+        public static void TestUnBoxing()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(ASTERISK + "Begin display Unboxing" + ASTERISK);
+            int num = 23;
+            Object obj = num;   //Boxing - converting a Value Type to a Reference Type
+            Console.WriteLine("Value type - value {0}", num);
+            Console.WriteLine("Reference type - value {0} through Boxing", obj);
+            obj = 50;
+            int num2 = (int)obj; //Unboxing - converting a Reference Type (object) to a Value Type (int, char etc)
+            Console.WriteLine("Reference type - new value {0} through assignment", obj);
+            Console.WriteLine("Value type - value {0} through Unboxing", obj);
+
+            Console.WriteLine("");
+            Console.WriteLine(ASTERISK + "End display Unboxing" + ASTERISK);
+
+        }
+
+        //Left operands value is moved left by the number of bits specified by the right operand
+        // x << y is equivalent to x multiply 2^y
+        //Left operands value is moved right by the number of bits specified by the rigth operand
+        // x >> y is equivalent to x divide by 2^y
+
+        public static void TestBitWiseShift()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(ASTERISK + "Begin display Left Shift/Rigth Shift" + ASTERISK);
+
+            int leftOperand = 60; // 0 0 1 1 1 1 0 0
+            int c = 0;
+
+            c = leftOperand >> 2; //shift right, expect c --> 13
+
+            Console.WriteLine("Left operand value: {0}", leftOperand);
+            Console.WriteLine("Left shift - shift by 2. Operand new value: {0}", c);
+
+            c = leftOperand << 2; //shift left, expect c--> 240
+            Console.WriteLine("Right operand value: {0}", leftOperand);
+            Console.WriteLine("Right shift - shift by 2. Operand new value: {0}", c);
+
+            Console.WriteLine("");
+            Console.WriteLine(ASTERISK + "End display Left Shift/Rigth Shift" + ASTERISK);
+
+        }
+
+
+        public static void TestParity()
+        {
+            Random rand = new Random();
+            //int num = 13;
+            int num = rand.Next(2, 100);
+            bool isOdd = ParityCheck(num);
+
+            Console.WriteLine("");
+            Console.WriteLine("Random number - value: {0}, contain odd/even number of 1-bits '{1}'", num, (isOdd ? "odd" : "even"));
+
+
+        }
+
+        //Parity: Parity of a number refers to whether it contains an odd or even number of 1-bits.
+        //The number has "odd parity", if it contains odd number of 1-bits and is "even parity" if it contains
+        //even number of 1-bits.
+        //Main idea of the below solutions is - loop while n is not 0 and in loop unset one of
+        //the set bits and invert parity
+        public static bool ParityCheck(int n)
+        {
+            bool parity = false;
+            while (n != 0)
+            {
+                parity = !parity; //invert parity
+                n = n & (n - 1);
+            }
+
+            return parity;
+
+        }
+
     }
 }
